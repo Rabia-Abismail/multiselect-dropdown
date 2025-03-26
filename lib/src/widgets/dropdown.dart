6c4e77cfed69e15ec77ca58015cfb10a5b381a23
Rabia-Abismail <rabia.abismail@gmail.com>
2025-03-26 11:03:22 +0100

    fixed listview scroll issue when multi_dropdown is placed in another scrollable widget...

--- lib/src/widgets/dropdown.dart
+++ lib/src/widgets/dropdown.dart
@@ -58,12 +58,9 @@ class _Dropdown<T> extends StatelessWidget {
 
   int get _selectedCount => items.where((element) => element.selected).length;
 
-  static const Map<ShortcutActivator, Intent> _webShortcuts =
-      <ShortcutActivator, Intent>{
-    SingleActivator(LogicalKeyboardKey.arrowDown):
-        DirectionalFocusIntent(TraversalDirection.down),
-    SingleActivator(LogicalKeyboardKey.arrowUp):
-        DirectionalFocusIntent(TraversalDirection.up),
+  static const Map<ShortcutActivator, Intent> _webShortcuts = <ShortcutActivator, Intent>{
+    SingleActivator(LogicalKeyboardKey.arrowDown): DirectionalFocusIntent(TraversalDirection.down),
+    SingleActivator(LogicalKeyboardKey.arrowUp): DirectionalFocusIntent(TraversalDirection.up),
   };
 
   @override
@@ -107,12 +104,13 @@ class _Dropdown<T> extends StatelessWidget {
                   removeBottom: true,
                   removeLeft: true,
                   removeRight: true,
-                  child: ListView.separated(
-                    separatorBuilder: (_, __) =>
-                        itemSeparator ?? const SizedBox.shrink(),
-                    shrinkWrap: true,
-                    itemCount: items.length,
-                    itemBuilder: (_, int index) => _buildOption(index, theme),
+                  child: PrimaryScrollController.none(
+                    child: ListView.separated(
+                      separatorBuilder: (_, __) => itemSeparator ?? const SizedBox.shrink(),
+                      shrinkWrap: true,
+                      itemCount: items.length,
+                      itemBuilder: (_, int index) => _buildOption(index, theme),
+                    ),
                   ),
                 ),
               ),
@@ -172,13 +170,10 @@ class _Dropdown<T> extends StatelessWidget {
         selected: option.selected,
         visualDensity: VisualDensity.adaptivePlatformDensity,
         focusColor: dropdownItemDecoration.backgroundColor?.withAlpha(100),
-        selectedColor: dropdownItemDecoration.selectedTextColor ??
-            theme.colorScheme.onSurface,
-        textColor:
-            dropdownItemDecoration.textColor ?? theme.colorScheme.onSurface,
+        selectedColor: dropdownItemDecoration.selectedTextColor ?? theme.colorScheme.onSurface,
+        textColor: dropdownItemDecoration.textColor ?? theme.colorScheme.onSurface,
         tileColor: tileColor ?? Colors.transparent,
-        selectedTileColor: dropdownItemDecoration.selectedBackgroundColor ??
-            Colors.grey.shade200,
+        selectedTileColor: dropdownItemDecoration.selectedBackgroundColor ?? Colors.grey.shade200,
         onTap: () {
           if (option.disabled) return;
 
@@ -194,9 +189,7 @@ class _Dropdown<T> extends StatelessWidget {
   void _onSearchChange(String value) => onSearchChange?.call(value);
 
   bool _reachedMaxSelection(DropdownItem<dynamic> option) {
-    return !option.selected &&
-        maxSelections > 0 &&
-        _selectedCount >= maxSelections;
+    return !option.selected && maxSelections > 0 && _selectedCount >= maxSelections;
   }
 }
 
